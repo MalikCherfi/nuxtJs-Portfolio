1298ddc2d0c40fde933f44cbf8a961bb0bcc8d4a
Malik Cherfi <malikcherfi@gmail.com>
2022-06-16 14:40:02 +0200

    add style and autofill

--- pages/Admin/index.vue
+++ pages/Admin/index.vue
@@ -136,7 +136,8 @@
                     @click="
                       (modalUpdate = true),
                         (modalChooseProjectUpdate = false),
-                        (idToUpdate = project.id)
+                        (idToUpdate = project.id),
+                        resetForm(project)
                     "
                     color="primary"
                     >Modifier</mdb-btn
@@ -159,38 +160,104 @@
 
         <form
           enctype="multipart/form-data"
-          @submit.prevent="updateProject(idToUpdate)"
+          @submit="updateProject(idToUpdate), (modalUpdate = false)"
         >
           <mdb-modal-body class="mx-3 grey-text">
             <mdb-input
-              v-model="title"
+              name="title"
+              v-model="formValue.title"
               label="Nom du projet"
               type="text"
               class="mb-5"
             />
-            <mdb-input v-model="link" label="Lien vers le projet" type="text" />
-            <mdb-input v-model="details" label="Description" type="textarea" />
-            <mdb-input v-model="years" label="Année" type="number" />
-            <input
-              type="file"
-              name="image.principal"
-              @change="processPrincipalFile($event)"
+            <mdb-input
+              name="link"
+              v-model="formValue.link"
+              label="Lien vers le projet"
+              type="text"
             />
-            <input
-              style="margin-top: 20px"
-              multiple
-              type="file"
-              name="image.secondary"
-              @change="processSecondaryFile($event)"
+            <mdb-input
+              name="details"
+              v-model="formValue.details"
+              label="Description"
+              type="textarea"
             />
+            <mdb-input
+              name="years"
+              v-model="formValue.years"
+              label="Année"
+              type="number"
+            />
+            <!-- Input File Principal Img-->
+            <div class="w-100 d-flex justify-content-center">
+              <label
+                for="file1"
+                style="
+                  cursor: pointer;
+
+                  padding: 2em;
+                  border-radius: 0.25em;
+                  box-shadow: 0 0 1em rgba(0, 0, 0, 0.25);
+                "
+              >
+                <div class="d-flex justify-content-around align-items-center">
+                  <img
+                    class="w-25"
+                    style="height: auto"
+                    v-if="imgPrincipalShow != null"
+                    :src="imgPrincipalShow"
+                  />
+                  <p style="margin: 0px">Sélectionner une image</p>
+                </div>
+                <input
+                  style="display: none"
+                  type="file"
+                  name="image.principal"
+                  id="file1"
+                  @change="processPrincipalFile($event), readImage($event)"
+                />
+              </label>
+            </div>
+            <!-- /Input File Principal Img-->
+
+            <!-- Input File Secondary Img -->
+            <div class="w-100 d-flex justify-content-center">
+              <label
+                for="file2"
+                style="
+                  cursor: pointer;
+                  padding: 2em;
+                  border-radius: 0.25em;
+                  box-shadow: 0 0 1em rgba(0, 0, 0, 0.25);
+                "
+              >
+                <div
+                  class="d-flex justify-content-around align-items-center flex-wrap"
+                >
+                  <img
+                    :key="index"
+                    v-for="(img, index) in imgSecondaryShow"
+                    class="w-25"
+                    style="height: auto; margin: 5px"
+                    :src="img"
+                  />
+                  <p style="margin: 0px">Sélectionner une image</p>
+                </div>
+                <input
+                  style="margin-top: 20px; display: none"
+                  multiple
+                  type="file"
+                  name="image.secondary"
+                  id="file2"
+                  @change="processSecondaryFile($event), readImage($event)"
+                />
+              </label>
+            </div>
+            <!-- /Input File Secondary Img -->
           </mdb-modal-body>
 
           <mdb-modal-footer center>
-            <input
-              @click="modalUpdate = false"
-              class="btn btn-unique"
-              type="submit"
-            />
+            <input class="btn btn-unique" type="submit" />
           </mdb-modal-footer>
         </form>
       </mdb-modal>
@@ -353,6 +420,9 @@ export default {
       modalChooseProjectUpdate: false,
       modalUpdate: false,
       idToUpdate: null,
+      formValue: {},
+      imgPrincipalShow: null,
+      imgSecondaryShow: [],
     };
   },
 
@@ -367,6 +437,39 @@ export default {
   },
 
   methods: {
+    resetForm(project) {
+      this.formValue.title = project.title;
+      this.formValue.link = project.link;
+      this.formValue.details = project.details;
+      this.formValue.years = project.years;
+    },
+
+    readImage(event) {
+      // Check if the file is an image.
+      if (
+        event.target.files[0].type &&
+        !event.target.files[0].type.startsWith("image/")
+      ) {
+        console.log("File is not an image.", event.target.files[0].type, event);
+        return;
+      }
+
+      console.log(event.target.id);
+
+      const reader = new FileReader();
+      reader.addEventListener("load", (e) => {
+        if (event.target.id === "file1") {
+          this.imgPrincipalShow = e.target.result;
+        } else {
+          for (let index = 0; index < event.target.files.length; index++) {
+            this.imgSecondaryShow.push(e.target.result);
+          }
+        }
+      });
+      reader.readAsDataURL(event.target.files[0]);
+      console.log(reader);
+    },
+
     processPrincipalFile(event) {
       this.principalImg = event.target.files[0];
     },
@@ -375,7 +478,6 @@ export default {
       this.secondaryImg.push(event.target.files[0]);
     },
     postProject() {
-      console.log("salut");
       const imgData = new FormData();
 
       imgData.append("image.principal", this.principalImg);
@@ -438,6 +540,13 @@ export default {
     },
 
     updateProject(id) {
+      var data = {
+        title: this.formValue.title,
+        years: parseInt(this.formValue.years),
+        link: this.formValue.link,
+        details: this.formValue.details,
+      };
+
       const imgData = new FormData();
 
       imgData.append("image.principal", this.principalImg);
@@ -445,12 +554,7 @@ export default {
         imgData.append("image.secondary", this.secondaryImg[index]);
       }
 
-      var data = {
-        title: this.title,
-        years: parseInt(this.years),
-        link: this.link,
-        details: this.details,
-      };
+      console.log(imgData);
 
       axios
         .post("/upload", imgData)
